0395ce9f2b709e886e1fc53e2453d18329a418a1
japark <wkd5878@gmail.com>
2024-09-12 19:04:34 +0900

    feat: 로그인 페이지 xml 작업

--- app/src/main/java/com/example/androidapplecation/ComponentHeader.java
+++ app/src/main/java/com/example/androidapplecation/ComponentHeader.java
@@ -1,23 +1,25 @@
 package com.example.androidapplecation;
-
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
-public class ComponentHeader extends Activity {
+public class ComponentHeader extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.component_header);
-    }
+        Button undoButton = findViewById(R.id.buttonId_undo);
+        System.out.println(undoButton);
 
-    @Override
-    public void onBackPressed() {
-        // 뒤로 가기 로직 구현
-        Toast.makeText(this, "뒤로 가기 버튼을 눌렀습니다.", Toast.LENGTH_SHORT).show();
-
-        // 예를 들어, 조건에 따라 액티비티를 종료하거나, 백 스택으로 돌아가게 할 수 있습니다.
-        super.onBackPressed();  // 기본 뒤로가기 동작을 실행하려면 이 줄을 호출
+        // 언두 버튼 클릭 시 뒤로 가기 구현
+        undoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 현재 액티비티 종료 (뒤로 가기 효과)
+                finish();
+            }
+        });
     }
 }
