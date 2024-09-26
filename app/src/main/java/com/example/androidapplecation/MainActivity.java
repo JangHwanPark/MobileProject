5800a671dbd9016c96854fe7e7455a28c91016ee
japark <wkd5878@gmail.com>
2024-09-26 11:18:00 +0900

    feat - 헤더 버튼 표시 여부 로직 추가

--- app/src/main/java/com/example/androidapplecation/MainActivity.java
+++ app/src/main/java/com/example/androidapplecation/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_user_account);
+        setContentView(R.layout.activity_create_post);
 
         // 모든 사용자를 가져와서 출력
         ArrayList<User> users = UserRepository.getInstance().getUserList();
--- app/src/main/java/com/example/androidapplecation/activity/CreatePostActivity.java
+++ app/src/main/java/com/example/androidapplecation/activity/CreatePostActivity.java
@@ -1,6 +1,8 @@
 package com.example.androidapplecation.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.androidapplecation.R;
 
@@ -10,5 +12,9 @@ public class CreatePostActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
+
+        // 체크 버튼 설정
+        Button submitBtn = findViewById(R.id.header_btn_check);
+        submitBtn.setVisibility(View.VISIBLE);
     }
 }
