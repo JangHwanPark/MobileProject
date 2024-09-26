b44a4734f0d0efff052893e440878acbb26cb39b
japark <wkd5878@gmail.com>
2024-09-26 14:29:49 +0900

    feat - 질문 리포지토리 생성

---
+++ app/src/main/java/com/example/androidapplecation/repository/QuestionRepository.java
@@ -0,0 +1,45 @@
+package com.example.androidapplecation.repository;
+import com.example.androidapplecation.model.Question;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class QuestionRepository {
+    private static QuestionRepository instance;
+    private ArrayList<Question> questionsList;
+
+    private QuestionRepository() {
+        questionsList = new ArrayList<>();
+        initializeQuestion();
+    }
+
+    public static QuestionRepository getInstance() {
+        if (instance == null) {
+            instance = new QuestionRepository();
+        }
+        return instance;
+    }
+
+    private void initializeQuestion() {
+        for (int i = 1; i <= 10; i++) {
+            Question question = new Question(
+                    "id" + i,
+                    "title" + i,
+                    "content" + i,
+                    "test user" + i,
+                    "qid" + i,
+                    new Date(),
+                    new Date()
+            );
+            questionsList.add(question);
+        }
+    }
+
+    public ArrayList<Question> getQuestionsList() {
+        return questionsList;
+    }
+
+    public void addQuestion(Question question) {
+        questionsList.add(question);
+    }
+}
