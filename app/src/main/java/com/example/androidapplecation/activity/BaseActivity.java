1557db656c85c23340a5a3c990ecdcdec85876f5
japark <wkd5878@gmail.com>
2024-10-24 11:51:42 +0900

    fix : QuestionDetailActivity 내 뒤로가기 기능 추가

--- app/src/main/java/com/example/androidapplecation/activity/BaseActivity.java
+++ app/src/main/java/com/example/androidapplecation/activity/BaseActivity.java
@@ -1,6 +1,7 @@
 package com.example.androidapplecation.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,17 +12,15 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.component_header);
+        //setContentView(R.layout.component_header);
      }
 
     protected void setupUndoButton() {
-        Button undoButton = findViewById(R.id.buttonId_undo);
-        undoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 뒤로 가기 버튼 클릭 처리
-                finish();
-            }
+        View headerView = findViewById(R.id.component_header); // 인클루드된 뷰 전체를 찾기
+        Button undoButton = headerView.findViewById(R.id.buttonId_undo); // 인클루드된 뷰 안의 버튼 찾기
+        undoButton.setOnClickListener(v -> {
+            Log.d("DetailsActivity", "뒤로 가기 버튼 클릭됨");
+            finish();
         });
     }
 }
--- app/src/main/java/com/example/androidapplecation/activity/QuestionDetailActivity.java
+++ app/src/main/java/com/example/androidapplecation/activity/QuestionDetailActivity.java
@@ -8,12 +8,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.androidapplecation.R;
 
-public class QuestionDetailActivity extends AppCompatActivity {
+public class QuestionDetailActivity extends BaseActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details); // XML 레이아웃 설정
+
+        // 버튼 클릭 시 뒤로 가기
+        setupUndoButton();
 
         // XML과 연결
         TextView textViewTitle = findViewById(R.id.textViewTitle);
